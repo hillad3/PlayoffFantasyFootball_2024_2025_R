a20b81443dd50de997768a90797e140270d36540
hillad3 <adam.x.hill@gmail.com>
2025-01-14 16:26:40 -0500

    Updated with refreshed stat files.

--- Scripts/03_Score Playoff Rosters.R
+++ Scripts/03_Score Playoff Rosters.R
@@ -11,7 +11,7 @@ library(nflreadr)
 library(openxlsx)
 
 roster_dir <- "App/data/"
-roster_file <- "Playoff Fantasy Rosters for 2024-2025, Compiled 2024-12-28 113246.csv"
+roster_file <- "Playoff Fantasy Rosters for 2024-2025, Compiled 2025-01-14 085019.csv"
 
 rosters <- fread(file = paste0(roster_dir,roster_file))
 rosters[,team_abbr:=NULL]
@@ -23,7 +23,7 @@ if("Fantasy Owner Email" %in% names(rosters)){
 }
 
 stats_dir <- "App/data/"
-stats_file_players <- "stats_2024_REG_POST_gen2025-01-05 212703.csv"
+stats_file_players <- "stats_2024_REG_POST_gen2025-01-14 160944.csv"
 stats_players <- fread(file = paste0(stats_dir,stats_file_players))
 stats_players[,player_id:=ifelse(position=="Defense",team_abbr,player_id)]
 
@@ -33,7 +33,7 @@ scored_rosters <- scored_rosters[stat_type=="fantasy_points"]
 
 output_file <- paste0(
   "App/data/NFL Fantasy Scores for 2024-2025 as of ",
-  str_remove_all(Sys.time(), ":"),
+  str_remove_all(str_sub(Sys.time(),1,19), ":"),
   ".csv"
 )
 
